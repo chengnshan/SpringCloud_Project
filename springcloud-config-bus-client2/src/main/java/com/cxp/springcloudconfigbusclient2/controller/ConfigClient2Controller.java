ce7a4d81806cdf625459015772b6be935622bca5
cheng <276629352@qq.com>
2019-07-28 15:22:30 +0800

    SpringCloud bus消息总线自动刷新配置 客户端2

---
+++ springcloud-config-bus-client2/src/main/java/com/cxp/springcloudconfigbusclient2/controller/ConfigClient2Controller.java
@@ -0,0 +1,32 @@
+package com.cxp.springcloudconfigbusclient2.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 程
+ * @date 2019/7/26 下午7:04
+ */
+@RestController
+@RefreshScope
+public class ConfigClient2Controller {
+
+    @Value(value = "${config.bus.client.name}")
+    private String configBusClientName;
+
+    @GetMapping("/getProfile")
+    public String getProfile() {
+        return "configClientName : "+this.configBusClientName;
+    }
+
+    public String getConfigClientName() {
+        return configBusClientName;
+    }
+
+    public void setConfigClientName(String configClientName) {
+        this.configBusClientName = configClientName;
+    }
+
+}
