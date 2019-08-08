45307d0b12e0c7f396d874729e496ae398f60631
cheng <276629352@qq.com>
2019-08-08 08:16:10 +0800

    SpringCloud中 消息生产者----添加h2内存数据库支持

--- springcloud-producer-h2database/src/main/java/com/cxp/springcloudproducerh2database/controller/TestH2DatabaseController.java
+++ springcloud-producer-h2database/src/main/java/com/cxp/springcloudproducerh2database/controller/TestH2DatabaseController.java
@@ -4,6 +4,7 @@ import com.cxp.springcloudproducerh2database.mapper.UserInfoMapper;
 import com.cxp.springcloudproducerh2database.pojo.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -21,7 +22,8 @@ public class TestH2DatabaseController {
     private UserInfoMapper userInfoMapper;
 
     @RequestMapping(value = "testH2DatabaseMethod")
-    public List<UserInfo> testH2DatabaseMethod(){
+    public List<UserInfo> testH2DatabaseMethod(@RequestParam Map<String,Object> map){
+        System.out.println("testH2DatabaseMethod in param : "+map);
         Map<String, Object> columnMap=new HashMap<>();
         columnMap.put("username","Jone");
         List<UserInfo> userInfos = userInfoMapper.selectByMap(columnMap);
